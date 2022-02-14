d8972d6ea6aca1f09a685335fac773920d2a21de
me <znamsiparolata4esno@abv.bg>
2022-02-14 13:37:12 +0200

    asd

--- good_stuff.mod
+++ good_stuff.mod
@@ -1,8 +1,8 @@
-version="0.4"
+version="0.5"
 tags={
 	"Balance"
 }
 name="good_stuff"
-supported_version="1.11.4"
+supported_version="1.11.5"
 path="/home/me/.local/share/Paradox Interactive/Hearts of Iron IV/mod/good_stuff"
 remote_file_id="2704894318"
--- good_stuff/common/defines/gucci_defines.lua
+++ good_stuff/common/defines/gucci_defines.lua
@@ -1,2 +1,7 @@
-NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 6900
+NDefines.NDiplomacy.TENSION_DECAY = 0.5 -- default 0.1
+NDefines.NDiplomacy.TENSION_SIZE_FACTOR = 0.8 -- default 1.0
+NDefines.NDiplomacy.TENSION_TIME_SCALE_MIN = 0 -- default 0.25
 
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 6900 -- default 500
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 6900 -- default 500
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 6900 -- default 500
