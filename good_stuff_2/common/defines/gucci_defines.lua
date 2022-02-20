bc5d2da736fdd343d18efec8d336d1dba6c25c53
me <znamsiparolata4esno@abv.bg>
2022-02-20 03:39:52 +0200

    asd

--- good_stuff_2/common/defines/gucci_defines.lua
+++ good_stuff_2/common/defines/gucci_defines.lua
@@ -1,14 +1,18 @@
--- TODO
---NDefines.NCountry.SPECIAL_FORCES_CAP_MIN = 1.0 -- default 0.05
---SPECIAL_FORCES_CAP_MIN = 0 -- default 24
 
-NDefines.NDiplomacy.TENSION_DECAY = 0.4 -- for some fucking reason setting this to 0.4 sets the decay to 0.5 -- default 0.1
+NDefines.NCountry.SPECIAL_FORCES_CAP_MIN = 500 -- default 24
+
+NDefines.NDiplomacy.TENSION_DECAY = 0.4 -- reports as 0.5, bit works properly -- default 0.1
 NDefines.NDiplomacy.TENSION_SIZE_FACTOR = 0.85 -- default 1.0
 NDefines.NDiplomacy.TENSION_TIME_SCALE_MIN = 0 -- default 0.25
 
+NDefines.NGame.GAME_SPEED_SECONDS = { 0.2, 0.1, 0.05, 0.03, 0.0 } -- default { 2.0, 0.5, 0.2, 0.1, 0.0 }
 NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 30 -- default 10
 NDefines.NGame.LAG_DAYS_FOR_PAUSE = 75 -- default 25
 
+NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 420 -- default 24
+NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 690 -- default 24
+NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 9 -- default 5
 NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 6900 -- default 500
 NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 6900 -- default 500
 NDefines.NMilitary.MAX_AIR_EXPERIENCE = 6900 -- default 500
+
