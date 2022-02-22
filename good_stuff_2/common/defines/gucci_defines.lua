0b7eeb2f7b6c060cb54db345cc2b96a814fc1235
me <znamsiparolata4esno@abv.bg>
2022-02-22 21:28:13 +0200

    asd

--- good_stuff_2/common/defines/gucci_defines.lua
+++ good_stuff_2/common/defines/gucci_defines.lua
@@ -5,7 +5,8 @@ NDefines.NDiplomacy.TENSION_DECAY = 0.25 -- reports as 0.5, bit works properly -
 NDefines.NDiplomacy.TENSION_SIZE_FACTOR = 0.8 -- default 1.0
 NDefines.NDiplomacy.TENSION_TIME_SCALE_MIN = 0 -- default 0.25
 
-NDefines.NGame.GAME_SPEED_SECONDS = { 0.25, 0.180666667, 0.111333334, 0.042, 0.0 } -- default { 2.0, 0.5, 0.2, 0.1, 0.0 }
+NDefines.NGame.GAME_SPEED_SECONDS = { 0.25, 0.1875, 0.125, 0.0625, 0.0 } -- default { 2.0, 0.5, 0.2, 0.1, 0.0 }
+-- TODO test on bad CPU for { 0.25, 0.180666667, 0.111333334, 0.042, 0.0 }
 -- pri 0.2 na X mu lagi late game-a (intel)
 -- pri 0.04 na X mu lagi ot vreme na vreme (intel)
 -- pri 0.03 na X pochva da mu lagi (na intela)
