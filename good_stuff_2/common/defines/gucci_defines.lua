0b163b01af20c1d5ae2bf228c0c556922082351e
me <znamsiparolata4esno@abv.bg>
2022-03-22 19:18:31 +0200

    asd

--- good_stuff_2/common/defines/gucci_defines.lua
+++ good_stuff_2/common/defines/gucci_defines.lua
@@ -5,7 +5,7 @@ NDefines.NDiplomacy.TENSION_DECAY = 0.25 -- reports as 0.5, bit works properly -
 NDefines.NDiplomacy.TENSION_SIZE_FACTOR = 0.8 -- default 1.0
 NDefines.NDiplomacy.TENSION_TIME_SCALE_MIN = 0 -- default 0.25
 
-NDefines.NGame.GAME_SPEED_SECONDS = { 0.61, 0.05, 0.04, 0.03, 0.0 } -- default { 2.0, 0.5, 0.2, 0.1, 0.0 }
+NDefines.NGame.GAME_SPEED_SECONDS = { 0.3, 0.05, 0.04, 0.03, 0.0 } -- default { 2.0, 0.5, 0.2, 0.1, 0.0 }
 --
 -- r5 5600x
 -- idkman its too fast
@@ -14,16 +14,17 @@ NDefines.NGame.GAME_SPEED_SECONDS = { 0.61, 0.05, 0.04, 0.03, 0.0 } -- default {
 -- 0.125 is too high
 -- 0.04 is too high
 --
--- i5 1st gen
--- 0.75 is too high
--- 0.05 is good eraly game, but too low for late game
+-- i5 750
+-- 0.61 is too high
+-- 0.3 - testing
+-- 0.05 is good eraly game, but too low for mid to late game
 --
 -- smt idk
 -- { 0.25, 0.09375, 0.0625, 0.03125, 0.0 }
 -- { 0.25, 0.1875, 0.125, 0.0625, 0.0 }
 
-NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 20 -- default 10
-NDefines.NGame.LAG_DAYS_FOR_PAUSE = 50 -- default 25
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 168 -- default 10
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 1440 -- default 25
 
 NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 4 -- default 20
 NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 1 -- default 5
@@ -33,5 +34,5 @@ NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 512 -- setting this to 0 brakes
 NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 0 -- default 5
 NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 6900 -- default 500
 NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 6900 -- default 500
-NDefines.NMilitary.MAX_AIR_EXPERIENCE = 6907 -- default 500
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 6909 -- default 500
 
