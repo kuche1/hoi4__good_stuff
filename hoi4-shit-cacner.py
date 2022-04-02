b8810f3436e0c0430ed4bb3a01e802dbae635cc8
me <znamsiparolata4esno@abv.bg>
2022-04-02 18:23:03 +0300

    asd

---
+++ hoi4-shit-cacner.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+
+import os
+import sys
+
+dir_source = '/home/me/.steam/steam/steamapps/common/Hearts of Iron IV/history/states/'
+dir_dest = '/home/me/hoi4-mod-folder/disable_asia/history/states/'
+owner_old = 'owner = ' + sys.argv[1]
+owner_new = 'owner ='
+
+for dir_, fols, fils in os.walk(dir_source):
+    for fil in fils:
+        with open(dir_source+fil) as f:
+            cont = f.read()
+            if owner_old in cont:
+                print('yes')
+                cont = cont.replace(owner_old, owner_new)
+                with open(dir_dest+fil, 'w') as f:
+                    f.write(cont)
+    break
