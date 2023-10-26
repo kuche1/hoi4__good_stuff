7ea868a2fa6819d2e03537a9aae3da74a825e718
kuche1 <znamsiparolata4esno@abv.bg>
2023-10-26 20:08:05 +0300

    update

--- copy-to-local-mods-folder.sh
+++ copy-to-local-mods-folder.sh
@@ -3,17 +3,21 @@
 rm -rf ~/".local/share/Paradox Interactive/Hearts of Iron IV/mod/"good_stuff_2*
 cp -r good_stuff_2* ~/".local/share/Paradox Interactive/Hearts of Iron IV/mod/"
 
-# rm -rf ~/".local/share/Paradox Interactive/Hearts of Iron IV/mod/"more_technologies*
-# cp -r more_technologies* ~/".local/share/Paradox Interactive/Hearts of Iron IV/mod/"
+rm -rf ~/".local/share/Paradox Interactive/Hearts of Iron IV/mod/"more_technologies*
+cp -r more_technologies* ~/".local/share/Paradox Interactive/Hearts of Iron IV/mod/"
 
 # rm -rf ~/".local/share/Paradox Interactive/Hearts of Iron IV/mod/"epic_sound_mod*
 # cp -r epic_sound_mod* ~/".local/share/Paradox Interactive/Hearts of Iron IV/mod/"
 
-# rm -rf ~/".local/share/Paradox Interactive/Hearts of Iron IV/mod/"optimise_map*
-# cp -r optimise_map* ~/".local/share/Paradox Interactive/Hearts of Iron IV/mod/"
+rm -rf ~/".local/share/Paradox Interactive/Hearts of Iron IV/mod/"optimise_map*
+cp -r optimise_map* ~/".local/share/Paradox Interactive/Hearts of Iron IV/mod/"
 
-rm -rf ~/".local/share/Paradox Interactive/Hearts of Iron IV/mod/"factory_economy*
-cp -r factory_economy* ~/".local/share/Paradox Interactive/Hearts of Iron IV/mod/"
+#rm -rf ~/".local/share/Paradox Interactive/Hearts of Iron IV/mod/"factory_economy*
+#cp -r factory_economy* ~/".local/share/Paradox Interactive/Hearts of Iron IV/mod/"
+
+
+rm -rf ~/".local/share/Paradox Interactive/Hearts of Iron IV/mod/"speed-buttons-adjustment-for-5600g*
+cp -r speed-buttons-adjustment-for-5600g* ~/".local/share/Paradox Interactive/Hearts of Iron IV/mod/"
 
 ##################
 
