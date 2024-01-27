3346e4d198fb2598b14cd2dadaa302ed64a768ca
kuche1 <znamsiparolata4esno@abv.bg>
2024-01-28 00:02:37 +0200

    update

--- increase_consumer_goods/common/defines/increase_consumer_goods_defines.lua
+++ increase_consumer_goods/common/defines/increase_consumer_goods_defines.lua
@@ -1,4 +1,4 @@
 
 -- minimum consumer goods
 -- should be default plus the additional amount
-NDefines.NProduction.MINIMUM_NUMBER_OF_FACTORIES_TAKEN_BY_CONSUMER_GOODS_PERCENT = 0.25 -- default 0.1
+NDefines.NProduction.MINIMUM_NUMBER_OF_FACTORIES_TAKEN_BY_CONSUMER_GOODS_PERCENT = 0.2 -- default 0.1
