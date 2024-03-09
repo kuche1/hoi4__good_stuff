0d315250fc8dd3c4c21a73c82eda5d1fca2caf4d
kuche1 <znamsiparolata4esno@abv.bg>
2024-03-09 13:05:38 +0200

    update

--- increase_consumer_goods/common/defines/increase_consumer_goods_defines.lua
+++ increase_consumer_goods/common/defines/increase_consumer_goods_defines.lua
@@ -1,4 +1,9 @@
 
 -- minimum consumer goods
 -- should be default plus the additional amount
-NDefines.NProduction.MINIMUM_NUMBER_OF_FACTORIES_TAKEN_BY_CONSUMER_GOODS_PERCENT = 0.2 -- default 0.1
+--NDefines.NProduction.MINIMUM_NUMBER_OF_FACTORIES_TAKEN_BY_CONSUMER_GOODS_PERCENT = NDefines.NProduction.MINIMUM_NUMBER_OF_FACTORIES_TAKEN_BY_CONSUMER_GOODS_PERCENT + 0.05 -- default 0.1
+
+-- mil and doc output
+--NDefines.NProduction.BASE_FACTORY_SPEED = 5 -- default 5 -- make civs produce less
+NDefines.NProduction.BASE_FACTORY_SPEED_MIL = NDefines.NProduction.BASE_FACTORY_SPEED_MIL * 0.6 -- default 4.5 -- make mils produce less
+NDefines.NProduction.BASE_FACTORY_SPEED_NAV = NDefines.NProduction.BASE_FACTORY_SPEED_NAV * 0.6 -- default 2.5 -- make docs produce less
