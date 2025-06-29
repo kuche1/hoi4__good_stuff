737b084791a0cd42204c360a34363e9b896d7e09
kuche1 <znamsiparolata4esno@abv.bg>
2025-06-29 13:04:23 +0300

    update

---
+++ template/common/defines/template_defines.lua
@@ -0,0 +1,5 @@
+
+-- -- template editing cost
+-- NDefines.NMilitary.BASE_DIVISION_BRIGADE_GROUP_COST = 2 -- default 20
+-- NDefines.NMilitary.BASE_DIVISION_BRIGADE_CHANGE_COST = 2 -- default 5
+-- NDefines.NMilitary.BASE_DIVISION_SUPPORT_SLOT_COST = 2 -- default 10
