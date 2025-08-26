1504d974d2b3ae371b0db993a0069948090fe8e1
kuche1 <znamsiparolata4esno@abv.bg>
2025-08-26 17:39:42 +0300

    update

--- compliance/common/defines/compliance_defines.lua
+++ compliance/common/defines/compliance_defines.lua
@@ -1,15 +1,15 @@
 
 -- base compliance growth
-NDefines.NResistance.COMPLIANCE_GROWTH_BASE = NDefines.NResistance.COMPLIANCE_GROWTH_BASE * 1.2
+NDefines.NResistance.COMPLIANCE_GROWTH_BASE = NDefines.NResistance.COMPLIANCE_GROWTH_BASE * 1.1
 
 -- compliance growth when at piece
 -- `10` stands for `10%`
 -- default 10
-NDefines.NResistance.COMPLIANCE_GROWTH_IS_AT_PEACE = NDefines.NResistance.COMPLIANCE_GROWTH_IS_AT_PEACE * 1.2
+NDefines.NResistance.COMPLIANCE_GROWTH_IS_AT_PEACE = NDefines.NResistance.COMPLIANCE_GROWTH_IS_AT_PEACE * 1.1
 
 -- compliance growth if claim
 -- default 5
-NDefines.NResistance.COMPLIANCE_GROWTH_HAS_CLAIM = NDefines.NResistance.COMPLIANCE_GROWTH_HAS_CLAIM * 1.2
+NDefines.NResistance.COMPLIANCE_GROWTH_HAS_CLAIM = NDefines.NResistance.COMPLIANCE_GROWTH_HAS_CLAIM * 1.1
 
 -- wtf does this even do? the following comment is not true:
 -- `-0.5` stands for loose half your compliance on controller change (this does include changing ideology)
