f943592cf06e12bba942535adcdc763e16ad7d47
kuche1 <znamsiparolata4esno@abv.bg>
2026-02-14 22:55:11 +0200

    update

--- mods/army_upkeep/common/defines/army_upkeep_defines.lua
+++ mods/army_upkeep/common/defines/army_upkeep_defines.lua
@@ -1,4 +1,5 @@
 
 -- "Constant attrition value applied to armies"
 -- this causes equipment loss
-NDefines.NMilitary.UNIT_UPKEEP_ATTRITION = 0.01
+NDefines.NMilitary.UNIT_UPKEEP_ATTRITION = 0.02
+-- 0.01 can still cause a lot of lag in late game (due to the very high number of units)
